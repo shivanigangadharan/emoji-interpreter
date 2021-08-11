64260e7b9b931c8214966b64ee7d11ae5b076aed
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-08-12 00:32:52 +0530

    finished - all features done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
+
 function App() {
+  const [meaning, setMeaning] = useState("Or, click on any of the emojis below. Meaning of emoji will appear here.");
+  const emojis = {
+    "😁": "grin",
+    "😄": "laugh",
+    "😉": "wink",
+    "😎": "cool",
+    "😠": "angry",
+    "🚀": "rock8!"
+  };
+
+  var myEmojis = Object.keys(emojis);
+
+  function handleInput(ip) {
+    console.log(emojis[ip]);
+    if (emojis[ip] == undefined) {
+      setMeaning("Oops! That emoji doesn't exist in our dictionary, try something else?")
+    }
+    else {
+      setMeaning("That means " + emojis[ip] + ".");
+    }
+  }
+
+  function handleClick(em) {
+    console.log(emojis[em]);
+    setMeaning("That means " + emojis[em] + ".");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Emoji interpreter </h1>
+      <form>
+        <input onChange={(e) => { handleInput(e.target.value) }} className="inputText" placeholder="Enter an emoji here to know its meaning." type="text" />
+        {/* <input className="btn-search" type="submit" value="Search" /> */}
+      </form>
+      <h4> {meaning} </h4>
+      <div className="emojiList">
+        {
+          myEmojis.map((e) => {
+            return (
+              <div>
+                <span className="emo" onClick={() => handleClick(e)} key={e} >
+                  {e}
+                </span>
+              </div>
+            )
+          })
+        }
+
+
+      </div>
     </div>
   );
 }
